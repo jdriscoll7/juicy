745c92c65b5795dab01d231229d099975ee93c3e
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2019-02-05 16:51:23 -0600

    Create accelerometer_test.py

---
+++ code/test/accelerometer_test.py
@@ -0,0 +1,12 @@
+import utime
+from machine import I2C, Pin
+from mpu9250 import MPU9250
+
+i2c = I2C(scl=Pin(22), sda=Pin(21))
+sensor = MPU9250(i2c)
+
+print("MPU9250 id: " + hex(sensor.whoami))
+
+while True:
+
+    print(sensor.acceleration)
