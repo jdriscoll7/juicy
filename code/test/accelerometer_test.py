5df4113405e282833604cca742d5e54bb33fa990
jwd0023 <jwd0023@auburn.edu>
2019-02-13 00:57:23 -0600

    Sensor data visualization (live 3D plot!).

--- code/test/accelerometer_test.py
+++ code/test/accelerometer_test.py
@@ -1,7 +1,46 @@
-from mpu9250.mpu9250 import mpu9250
+from code.custom_libs.mpu9250.mpu9250 import mpu9250
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from collections import deque
 
+
+# If true, then accelerometer readings will be plotted. Will not be plotted if false.
+PLOT = True
+
+# Initialize sensor with external library class.
 sensor = mpu9250()
 
-while True:
 
-    print(sensor.accel)
+if __name__ == "__main__":
+
+    # Setup plotting if necessary.
+    if PLOT is True:
+
+        # 3D scatter plot setup.
+        fig2 = plt.figure()
+        ax2 = Axes3D(fig2)
+        ax2.set_xlim(-10, 10)
+        ax2.set_ylim(-10, 10)
+        ax2.set_zlim(-10, 10)
+        plot2 = ax2.scatter3D([], [], [])
+        plt.pause(1)
+
+        # Buffer to hold points we want to show.
+        points_to_show = [deque(maxlen=10), deque(maxlen=10), deque(maxlen=10)]
+
+    while True:
+
+        sensor_data = sensor.accel
+
+        print(sensor_data)
+
+        # Update the plot if plotting is turned on.
+        if PLOT is True:
+            points_to_show[0].append(sensor_data[0])
+            points_to_show[1].append(sensor_data[1])
+            points_to_show[2].append(sensor_data[2])
+            frame = ax2.scatter3D(*points_to_show, color='blue')
+
+            plt.pause(0.00000001)
+            frame.remove()
--- code/test/sensor_visualization.py
+++ code/test/sensor_visualization.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from collections import deque
 
 
 def a_random_sequence():
@@ -13,11 +15,30 @@ if __name__ == "__main__":
     i = 0
 
     # Some setup to make rendering of plot faster.
-    #fig, ax = plt.subplot()
+    fig2 = plt.figure()
+    ax2 = Axes3D(fig2)
+    ax2.set_xlim(-10, 10)
+    ax2.set_ylim(-10, 10)
+    ax2.set_zlim(-10, 10)
+    plot2 = ax2.scatter3D([], [], [])
+
+    plt.pause(1)
+
+    # Buffer to hold points we want to show.
+    points_to_show = [deque(maxlen=10), deque(maxlen=10), deque(maxlen=10)]
 
     # Plot a sequence sequentially (not all at once).
     for x in a_random_sequence():
-        plt.scatter(i, x)
+
+        points_to_show[0].append(i)
+        points_to_show[1].append(i)
+        points_to_show[2].append(x)
+        asd = ax2.scatter3D(*points_to_show, color='blue')
+
+        plt.pause(0.00000001)
+        asd.remove()
+
+
+        # Increment plot index for demo purposes.
         i = i + 1
 
-plt.show()
