06de6e18a4fc5582fbc365d0a11d405f2cc8c21b
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2019-02-22 15:54:56 -0600

    Update accelerometer_test.py
    clean up code and add comments

--- code/test/accelerometer_test.py
+++ code/test/accelerometer_test.py
@@ -1,29 +1,35 @@
+# Put library in python system path.
 import sys
 import os
 sys.path.append('../')
+
 #Setup GPIO 12 as OUTPUT
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(18, GPIO.IN)
-GPIO.output(12, 0)
 from juicy.mpu9250.mpu9250 import mpu9250
-#from matplotlib import pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from collections import deque
 
-arr = []
-arr = [0.0, 0.0, 0.0]
 
 # If true, then accelerometer readings will be plotted. Will not be plotted if false.
-PLOT =False 
-
-# Initialize sensor with external library class.
-sensor = mpu9250()
+PLOT = False 
 
 
 if __name__ == "__main__":
+
+    
+    # Initialize sensor with external library class.
+    sensor = mpu9250()
+    
+    # Marco's detection method setup.
+    arr = [0.0, 0.0, 0.0]
+    
+    # Setup GPIO modes and initial values.
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(18, GPIO.IN)
+    GPIO.output(12, 0)
 
     # Setup plotting if necessary.
     if PLOT is True:
@@ -40,33 +46,43 @@ if __name__ == "__main__":
         # Buffer to hold points we want to show.
         points_to_show = [deque(maxlen=10), deque(maxlen=10), deque(maxlen=10)]
 
-    last_mag = 0
+    # Initialize first magnitude to 10 - this was it won't trigger immediately every time...
+    last_mag = 10
 
     while True:
 
+        # Get sensor data.
         sensor_data = sensor.accel
-        print(sensor_data)
         
-        next_mag = np.linalg.norm(sensor_data)        
-
+        # Clear console before printing new information.
+        os.system('clear')
+        
+        # Print sensor data and the magnitude of the acceleration.
+        next_mag = np.linalg.norm(sensor_data)
+        print('sensor x: %3.3\nsensor y: %3.3f\nsensor z: %3.3f\n\nmagnitude: %3.3f', (*sensor_data, next_mag))
+   
+        # Thresholding for alarm detection - uncomment else for software LED/alarm reset.
         if 1.4*last_mag  < next_mag:
             GPIO.output(12, 1)
        # else:
        #     GPIO.output(12, 0)
 
-       
+        # Set the next value to compare.
         last_mag = next_mag
 
+       # Marco's detection method - uncomment to use this.
        # if ((sensor_data[0] - arr[0]) >= .5) | ((sensor_data[1] - arr[1]) >= .8) | ((sensor_data[2] - arr[2]) >= 1):
        #   GPIO.output(12, 1)
         
+        # Allows LED to be reset by external input (currently button).
         if GPIO.input(18) == 1:
-          GPIO.output(12, 0)
+            GPIO.output(12, 0)
+        
+        # Marco's detection method.
         arr[0] = sensor_data[0]
         arr[1] = sensor_data[1]
         arr[2] = sensor_data[2]
-        os.system('clear')
-
+       
         # Update the plot if plotting is turned on.
         if PLOT is True:
             points_to_show[0].append(sensor_data[0])
