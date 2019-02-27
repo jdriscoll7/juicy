dbe202d507760a55ca21e8229eeec461f1557ee9
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2019-02-26 22:19:30 -0600

    Update sensor_calibration.py

--- juicy/signal_processing/sensor_calibration.py
+++ juicy/signal_processing/sensor_calibration.py
@@ -81,7 +81,7 @@ class SensorModel:
         self.gyro_bias_fixed = True
         
         # Initialize moving average buffer with "moving_average_size" copies of the computed average.
-        self.moving_average_buffer = [self.gyro_bias] * self.moving_average_size
+        self.moving_average_buffer = [self.gyro_bias for i in range(self.moving_average_size)]
         
     def update_state(self, gyro, *args, **kwargs):
         """
