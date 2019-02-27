541fa44139787f22dbeaf859956a94384f9886df
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2019-02-26 21:18:04 -0600

    Update sensor_calibration.py

--- juicy/signal_processing/sensor_calibration.py
+++ juicy/signal_processing/sensor_calibration.py
@@ -95,7 +95,7 @@ class SensorModel:
         # 
         # Also correct for gyro error if possible.
         if self.gyro_bias_fixed is True:
-            self.orientation -= ((np.asarray(gyro) - self.gyro_bias) * dt * np.asarray([1, 1, -1]))
+            self.orientation -= ((np.asarray(gyro) - self.gyro_bias) * dt * np.asarray([-1, 1, -1]))
         else:
             self.orientation -= (np.asarray(gyro) * dt)
             print('Gyroscope error is not being fixed - uh oh.')
