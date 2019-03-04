97a7560e5a5cffac91cd1353914ceb824b4ec470
Joe Driscoll <32208193+jwd0023@users.noreply.github.com>
2019-03-04 17:29:50 -0600

    Update call_sender.py

--- juicy/alarm/call_sender.py
+++ juicy/alarm/call_sender.py
@@ -1,3 +1,37 @@
 # Send calls from this number: (251) 290-8658. (+12512908658)
 #
 # Uses Twilio API.
+
+
+from twilio.rest import Client
+import yaml
+
+
+class CallSender():
+    
+    def __init__(self, config_path):
+        """
+        Performs some basic initialization based on config file.
+       
+        Note: - Config file will probably have fixed location, and fields inside
+                of it will be changed by web interface or phone application.
+        """
+    
+        # Basically sets config file as a dict that is part of the object.
+        with open(config_path + "config.yml", 'r') as config_file:
+            self.settings = yaml.load(config_file)
+            
+        # Setup Twilio client based on API keys in config file.
+        self.client = Client(self.settings['api_keys']['account_sid'], 
+                             self.settings['api_keys']['auth_token'])
+
+    def send_calls(self):
+        """
+        Sends calls based on settings in configuration file.
+        """
+        
+        for recipient in self.settings['call_settings']['recipients']:
+            self.client.calls.create()
+            
+        
+        
