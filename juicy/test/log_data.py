5b65b9a423055cb0e7c25c81218e92549c90de7f
Joe Driscoll <32208193+jwd0023@users.noreply.github.com>
2019-04-08 13:52:54 -0500

    Update log_data.py

--- juicy/test/log_data.py
+++ juicy/test/log_data.py
@@ -17,7 +17,9 @@ def bulk_update_channel(write_data):
     data = json.dumps({'write_api_key': THINGSPEAK_API_KEY,
                        'updates'      : write_data})
     
-    data = urllib.parse.urlencode(json.loads(data)).encode('UTF-8')
+    data = urllib.parse.urlencode(json.loads(data[0:9])).encode('UTF-8')
+    
+    print(data)
     
     # Form the http request using urllib2.
     req = urllib.request.Request(url = REQUEST_URL)
